8ff0313132db332720f45bed8cbe90362c8aac67
LeeYuJoon <leeyujoon97@gmail.com>
2024-07-26 00:34:10 +0900

    refactor: ClubCreateRequest refactoring (#3)

--- src/main/java/project/backend/club/api/request/ClubCreateRequest.java
+++ src/main/java/project/backend/club/api/request/ClubCreateRequest.java
@@ -15,10 +15,10 @@ public class ClubCreateRequest {
     @NotBlank(message = "동아리명은 필수입니다.")
     private String clubName;
 
-    @NotBlank(message = "동아리 세션은 필수입니다.")
+    @NotNull(message = "동아리 세션은 필수입니다.")
     private List<String> sessions;
 
-    @NotBlank(message = "동아리 level은 필수입니다.")
+    @NotNull(message = "동아리 level은 필수입니다.")
     private List<String> levels;
 
     @NotNull(message = "작성자는 필수입니다.")
